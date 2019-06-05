270f255f3b009d2bb1564889fde76a318cd3336c
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-06-05 01:06:29 -0500

    implemented social icons and links.

--- my-site/src/Components/Body/Contact.js
+++ my-site/src/Components/Body/Contact.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faYoutube,
+  faGithub,
+  faTwitter,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
 const Wrapper = styled.div`
   background: rgb(0, 0, 0); /* The Fallback */
@@ -10,55 +18,45 @@ const Wrapper = styled.div`
   padding-bottom: 2%;
   min-height: 50vh;
 `;
-
-const FormWrap = styled.div`
+const SocialFollow = styled.div`
+  width: 100%;
+  padding-bottom: 2%;
   display: flex;
-  flex-direction: column;
-`;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top:5%;
 
-const SubmitButton = styled.input`
-  color: white;
-  background: none;
-  border-radius: 11px;
-  width: 10vw;
-  &:hover {
-    color: aqua;
-    transition: 0.3s;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.7);
-    border: aqua 1px solid;
+  > div {
+    width: 100%;
   }
 `;
-
-const InputField = styled.input`
-  border-radius: 11px;
-  background: none;
-  width: 12vw;
-  color:white;
-  }
-`;
-
-const InputWrapper = styled.label`
-  width:100%;
-  }
+const SocialIcon = styled.a`
+  padding: 1%;
 `;
 
 class Contact extends Component {
   render() {
     return (
       <Wrapper>
-        <FormWrap>
-          <form>
-            <InputWrapper>
-              Name:
-              <InputField type="text" name="name"/>
-            </InputWrapper>
-            <InputWrapper>
-              Email:
-              <InputField type="text" name="email" />
-            </InputWrapper>
-            <SubmitButton type="submit" value="Submit" />
-          </form>
-        </FormWrap>
+        <h2>Contact Info</h2>
+        <div>
+          <div>Email: josephhaydendevelopment@gmail.com</div>
+          <div>Location: Chicago, IL</div>
+          <SocialFollow>
+            <div>Follow me on social media!</div>
+            <div>
+              <SocialIcon href="https://github.com/JHaydenDev">
+                <FontAwesomeIcon icon={faGithub} size="3x" />
+              </SocialIcon>
+              <SocialIcon href="https://twitter.com/JHaydenDev">
+                <FontAwesomeIcon icon={faTwitter} size="3x" />
+              </SocialIcon>
+              <SocialIcon href="https://www.linkedin.com/in/joseph-hayden-b30a5b126/">
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+              </SocialIcon>
+            </div>
+          </SocialFollow>
+        </div>
       </Wrapper>
     );
   }
